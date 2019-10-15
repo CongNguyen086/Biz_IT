19a37192147d2955aff5a799f987db575fcaed4f
Nguyễn Thành Công <congntgcs16023@fpt.edu.vn>
2019-10-16 03:55:11 +0700

    Delete

--- node_modules/expo-facebook/build/Facebook.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type FacebookLoginResult = {
-    type: string;
-    token?: string;
-    expires?: number;
-};
-export declare type FacebookOptions = {
-    permissions?: string[];
-};
-export declare function logInWithReadPermissionsAsync(appId: string, options?: FacebookOptions): Promise<FacebookLoginResult>;
