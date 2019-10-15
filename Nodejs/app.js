a8d65180dd3cfcfcb3857a791786a28c59f90c41
Nguyễn Thành Công <congntgcs16023@fpt.edu.vn>
2019-10-16 02:31:33 +0700

    Re-arrangement

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -15,8 +15,8 @@ app.use(bodyParser.json());
 
 const connection = mysql.createConnection({
     host: '127.0.0.1',
-    user: 'BIT',
-    password: 'Khang20.09',
+    user: 'root',
+    password: 'sa123',
     database: 'bit_system',
     port: '3306',
     multipleStatements: true
---
+++ React Native/screens/HomePage.js
@@ -0,0 +1,120 @@
+import React, { Component, useState, useEffect } from 'react';
+import { StyleSheet, AsyncStorage, Text, View, SafeAreaView, Platform, Dimensions, ScrollView } from 'react-native';
+
+//Components
+import Header_HomePage from '../components/HomePage/Header_HomePage'
+import Categories_HomePage from '../components/HomePage/Categories_HomePage'
+import ProductsContainer from '../components/HomePage/ProductsContainer'
+
+const width = Dimensions.get('screen').width;
+const height = Dimensions.get('screen').height;
+
+export default function HomePage(props) {
+  const [timeData, setTimeData] = useState([])
+  const [recommendData, setRecommendData] = useState([])
+  const [popularData, setPopularData] = useState([])
+  
+  getTimeRecommendation = async () => {
+    try {
+      const token = await AsyncStorage.getItem('@userToken');
+      const response = await fetch(`http://192.168.1.4:3000/gettimerecommendationdeal?userId=${token}`);
+      const jsonData = await response.json();
+      setTimeData(jsonData[0])
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  getRecommendation = async () => {
+    try {
+      const token = await AsyncStorage.getItem('@userToken');
+      const response = await fetch(`http://192.168.1.4:3000/getrecommendeddeal?userId=${token}`);
+      const jsonData = await response.json();
+      setRecommendData(jsonData[0])
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  getPopular = async () => {
+    try {
+      const response = await fetch(`http://192.168.1.4:3000/getpopulardeal`);
+      const jsonData = await response.json();
+      setPopularData(jsonData[0])
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    getTimeRecommendation();
+    getRecommendation();
+    getPopular();
+  }, [])
+  return (
+    <SafeAreaView style={styles.safeAreaViewStyle}>
+      <View style={styles.container}>
+        <View style={styles.headerContaier}>
+          <Header_HomePage />
+        </View>
+        <ScrollView style={styles.bodyContainer} >
+          <View style={styles.categories}>
+            <Categories_HomePage />
+          </View>
+          <View style={styles.bodyElement}>
+            <ProductsContainer 
+              title='Dành riêng cho bạn'
+              data={timeData}
+              navigation={props.navigation}
+            />
+          </View>
+          <View style={styles.bodyElement}>
+            <ProductsContainer 
+              title='Có thể bạn hứng thú'
+              data={recommendData}
+              navigation={props.navigation}
+            />
+          </View>
+          <View style={styles.bodyElement}>
+            <ProductsContainer 
+              title='Phổ biến'
+              data={popularData}
+              navigation={props.navigation}
+            />
+          </View>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+HomePage.navigationOptions = {
+  header: null,
+};
+
+const styles = StyleSheet.create({
+  safeAreaViewStyle: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
+    backgroundColor: '#325340',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#EDEEEE',
+    alignItems: 'center'
+  },
+  headerContaier: {
+    flex: 0.35
+  },
+  bodyContainer: {
+    flex: 0.65
+  },
+  categories: {
+    flex: 1 / 3,
+    height: height / 3.5,
+    backgroundColor: 'white',
+    borderRadius: 12,  
+    marginTop: 10,
+  },
+  bodyElement: {
+    flex: 1 / 3,
+    height: height / 2.8
+  }
+});
