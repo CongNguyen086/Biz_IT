6815981bef4feec04bc1865dd2bdf07d3e4aee67
Nguyễn Thành Công <congntgcs16023@fpt.edu.vn>
2019-10-19 20:24:11 +0700

    xxx

--- BIT_SQL18-10.sql
+++ BIT_SQL18-10.sql
@@ -1,23 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
---
--- Host: localhost    Database: bit_system
--- ------------------------------------------------------
--- Server version	8.0.17
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `categories`
---
 
 DROP TABLE IF EXISTS `categories`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -359,23 +340,6 @@ INSERT INTO `users` VALUES ('3646728529149882702','Cong','0123456789','123'),('8
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Dumping events for database 'bit_system'
---
-
---
--- Dumping routines for database 'bit_system'
---
-/*!50003 DROP PROCEDURE IF EXISTS `GetPopularDeal` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `GetPopularDeal`()
 BEGIN
 	SELECT 
@@ -398,21 +362,6 @@ WHERE
     recommendedCategory.categoryId = deal.categoryId
 ORDER BY recommendedCategory.`rank`;
 END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `GetRecommendedDeal` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `GetRecommendedDeal`(IN userId VARCHAR(20))
 BEGIN
 	SELECT 
@@ -434,22 +383,7 @@ FROM
 WHERE
     recommendedCategory.categoryId = deal.categoryId
 ORDER BY recommendedCategory.`point`;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `GetStorePromotion` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
+END
 CREATE DEFINER=`root`@`localhost` PROCEDURE `GetStorePromotion`(IN dealId INT(11))
 BEGIN
 	SELECT 
@@ -469,22 +403,7 @@ FROM
         promotions.dealId = dealId) AS dealsPromotion ON merchants.serviceId = dealsPromotion.serviceId) as merchantsPromotion
         ON 
 			stores.serviceId = merchantsPromotion.serviceId;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `GetTimeRecommendationDeal` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
+END
 CREATE DEFINER=`root`@`localhost` PROCEDURE `GetTimeRecommendationDeal`(IN userId VARCHAR(20), IN currentTime TIME)
 BEGIN
 	SELECT 
@@ -514,74 +433,4 @@ FROM
 WHERE
     recommendedCategory.categoryId = deal.categoryId
 ORDER BY recommendedCategory.`point`;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-
---
--- Final view structure for view `merchants_popular`
---
-
-/*!50001 DROP VIEW IF EXISTS `merchants_popular`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8mb4 */;
-/*!50001 SET character_set_results     = utf8mb4 */;
-/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `merchants_popular` AS select `mer`.`serviceId` AS `serviceId`,`mer`.`merchantName` AS `merchantName`,`mer`.`categoryId` AS `categoryId`,`mer`.`icon` AS `icon`,`mer`.`reviewImage` AS `reviewImage`,`popular`.`rank` AS `rank` from (`popular_merchants` `popular` join `merchants` `mer` on(((`popular`.`serviceId` = `mer`.`serviceId`) and (`mer`.`isPromoted` = 1)))) */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
-
---
--- Final view structure for view `merchants_recommendation`
---
-
-/*!50001 DROP VIEW IF EXISTS `merchants_recommendation`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8mb4 */;
-/*!50001 SET character_set_results     = utf8mb4 */;
-/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `merchants_recommendation` AS select `rec`.`userId` AS `userId`,`mer`.`serviceId` AS `serviceId`,`mer`.`merchantName` AS `merchantName`,`mer`.`categoryId` AS `categoryId`,`mer`.`icon` AS `icon`,`mer`.`reviewImage` AS `reviewImage`,`rec`.`point` AS `point` from (`recommendation` `rec` join `merchants` `mer` on(((`rec`.`serviceId` = `mer`.`serviceId`) and (`mer`.`isPromoted` = 1)))) */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
-
---
--- Final view structure for view `merchants_timerecommendation`
---
-
-/*!50001 DROP VIEW IF EXISTS `merchants_timerecommendation`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8mb4 */;
-/*!50001 SET character_set_results     = utf8mb4 */;
-/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `merchants_timerecommendation` AS select `rec`.`userId` AS `userId`,`rec`.`startTime` AS `startTime`,`rec`.`endTime` AS `endTime`,`mer`.`serviceId` AS `serviceId`,`mer`.`merchantName` AS `merchantName`,`mer`.`categoryId` AS `categoryId`,`rec`.`point` AS `point` from (`time_recommendation` `rec` join `merchants` `mer` on(((`rec`.`serviceId` = `mer`.`serviceId`) and (`mer`.`isPromoted` = 1)))) */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-10-19 19:37:23
+END 
