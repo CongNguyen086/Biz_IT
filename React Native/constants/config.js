25e427d251475c352445daa816815feffd0dab4b
Hieu Do <hieudm@dgroup.co>
2020-09-01 12:57:51 +0700

    change config

--- React Native/constants/config.js
+++ React Native/constants/config.js
@@ -1,5 +1,5 @@
 export default config = {
-	ROOT : 'http://192.168.100.56:3000',
+	ROOT : 'http://localhost:3000',
 	API_ID : '795764707522976',
 	API_KEY : 'AIzaSyC6xFmp7UzfruFlCKb8QWyDBvA3lxNcSlQ'
 }
