04a5bb8b91cd9a66fb659cd6b4bd92d5d2f53b94
Cong Nguyen <congnt@dgroup.co>
2020-09-21 01:35:19 +0700

    Modify export

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,17 +1,13 @@
 import MysqlClient from "../config/db";
 
-const getContactList = async (req, res) => {
+export const getContactList = async (req, res, error) => {
     try {
         const client = MysqlClient.getInstance().connection;
         const contactList = await client.select("userPhone").from("users");
         const phoneList = contactList.map(ele => ele.userPhone);
         
         res.status(200).json({ phoneList });
-    } catch (error) {
+    } catch (err) {
         res.status(500).json({ error });
     }
-}
-
-export {
-    getContactList
 }
