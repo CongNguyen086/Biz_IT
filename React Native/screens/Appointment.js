c7669f59f47fe05269125f0941c956d86b73a079
Hieu Do <minhhieu76qng@gmail.com>
2020-09-20 15:14:23 +0700

    custom style for tabbar

--- React Native/screens/Appointment.js
+++ React Native/screens/Appointment.js
@@ -1,17 +1,95 @@
 import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Dimensions } from 'react-native'
 import PendingAppointment from '../components/Appointment/PendingAppointment';
 import HeaderTitle from '../components/HeaderTitle';
+import {TabView, SceneMap, TabBar, TabBarItemProps, TabBarItem} from 'react-native-tab-view';
+import Colors from '../constants/Colors';
+
+const AppointmentListScreen = () => (
+  <View style={[styles.scene, { backgroundColor: '#ff4081' }]} />
+);
+
+const PendingAppointmentScreen = () => (
+  <View style={styles.card}>
+    <PendingAppointment />
+  </View>
+);
+
+const initialLayout = { width: Dimensions.get('window').width };
 
 class Appointment extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      screenIndex: 0,
+      routes: [
+        { key: 'appointment_list', title: 'Appointment list' },
+        { key: 'pending_appointment', title: 'Pending appointment' },
+      ]
+    }
+  }
+
+  setIndex = index => {
+    this.setState({
+      screenIndex: index,
+    })
+  }
+
+  renderTabBarItem = props => {
+    console.log("Appointment -> state", JSON.stringify(props.navigationState))
+    console.log("Appointment -> route", JSON.stringify(props.route))
+    const currentRoute = props.route.key;
+    const activeRoute = props.navigationState.routes[props.navigationState.index].key;
+    const isActive = currentRoute === activeRoute;
+  
+    return (
+      <TabBarItem
+        {...props}
+        style={[styles.activeTab, isActive && {backgroundColor: '#fff'}]}
+      />
+    )
+  }
+
+  renderTabBar = (props) => {
+    return (
+      <TabBar
+        {...props}
+        renderTabBarItem={this.renderTabBarItem}
+        renderLabel={({ route, focused }) => {
+          const color = focused ? Colors.primary : '#fff';
+          return (
+            <Text style={{ color, fontSize: 16, fontWeight: '600' }}>
+              {route.title}
+            </Text>
+          )
+        }}
+        style={styles.tabBarContainer}
+        renderIndicator={() => null}
+      />
+    )
+  }
+
   render() {
+    const renderScene = SceneMap({
+      appointment_list: AppointmentListScreen,
+      pending_appointment: PendingAppointmentScreen,
+    });
+    const {routes, screenIndex} = this.state;
+    const {setIndex, renderTabBar} = this;
     return (
       <SafeAreaView style={{flex: 1, backgroundColor: '#EDEEEE',}}>
         <View style={styles.container}>
-          <View style={styles.navigationBar} />
-          <View style={styles.card}>
-            <PendingAppointment />
-          </View>
+          <TabView
+            navigationState={{ index: screenIndex, routes }}
+            renderScene={renderScene}
+            onIndexChange={setIndex}
+            initialLayout={initialLayout}
+            sceneContainerStyle={{
+              paddingTop: 30,
+            }}
+            renderTabBar={renderTabBar}
+          />
         </View>
       </SafeAreaView>
     )
@@ -26,6 +104,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
+    paddingTop: 30,
   },
   navigationBar: {
     backgroundColor: 'red',
@@ -37,6 +116,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     padding: 10,
     borderRadius: 3,
+  },
+  tabBarContainer: {
+    backgroundColor: '#C4C4C4',
+    borderRadius: 30,
+  },
+  activeTab: {
+    borderRadius: 30,
+    backgroundColor: 'transparent',
+    flex: 1,
+    margin: 2,
   }
 })
 
