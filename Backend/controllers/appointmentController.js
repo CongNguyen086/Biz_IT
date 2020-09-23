de3d23f9f975b7ba47a4c717d9364641ca829dbf
Cong Nguyen <congnt@dgroup.co>
2020-09-23 20:30:36 +0700

    Refactor appointment services

--- Backend/controllers/appointmentController.js
+++ Backend/controllers/appointmentController.js
@@ -1,24 +1,13 @@
 import { Appointment } from "../models"
-import { AppointmentService } from "../services/appointmentService"
+import AppointmentService from "../services/appointmentService"
 
 const appointmentService = new AppointmentService();
-export const proposeAppointment = async (req, res, error) => {
+export const proposeAppointment = async (req, res) => {
     try {
-        const { userId, stores, members, eventName, date } = req.body;
-        const appointment = new Appointment(
-            null,
-            eventName,
-            date,
-            userId
-        );
-        const appointmentId = await appointmentService.createAppointment(appointment);
-        await appointmentService.createAppointmentStores(stores, appointmentId);
-        Promise.all(members.map(async (memberId) => {
-            await appointmentService.upsertAppointmentMembers(memberId);
-        }));
-        
-        res.status(200).json({ appointmentId });
-    } catch (err) {
+        const newAppointmentId = await appointmentService.createNewAppointmentInfo(req.body);
+        res.status(200).json({ newAppointmentId });
+    } catch (error) {
+        console.log(error);
         res.status(500).json({ error });
     }
 }
--- Backend/services/appointmentService.js
+++ Backend/services/appointmentService.js
@@ -1,32 +1,81 @@
 import MysqlClient from "../config/db";
 
-export class AppointmentService {
+export default class AppointmentService {
     constructor() {
         this.client = MysqlClient.getInstance();
     }
 
     async createAppointment(appointment) {
-        const knex = this.client.connection;
-        const [id] = await knex.insert(appointment).into("appointments");
-        return id;
-    }
-
-    createAppointmentStores(storeIds, appointmentId) {
-        const knex = this.client.connection;
-        const data = [];
-        for (const storeId of storeIds) {
-            data.push({ storeId, appointmentId });
+        try {
+            const knex = this.client.connection;
+            const [id] = await knex.insert(appointment).into("appointments");
+            return id;
+        } catch (error) {
+            throw new Error(error);
         }
-        return knex.insert(data).into("appointment_stores");
     }
 
-    upsertAppointmentMembers(memberId, patch) {
-        const knex = this.client.connection;
-        if (patch) {
-            // TODO: Update member status
-            return;
+    async createAppointmentStores(storeIds, appointmentId) {
+        try {
+            const knex = this.client.connection;
+            const data = [];
+            for (const storeId of storeIds) {
+                data.push({ storeId, appointmentId });
+            }
+            return await knex.insert(data).into("appointment_stores");
+        } catch (error) {
+            throw new Error(error);
         }
-
-        return knex.insert({ memberId }).into("appointment_members");
     }
+
+    async upsertAppointmentMembers(memberId, appointmentId, patch) {
+        try {
+            const knex = this.client.connection;
+            if (patch) {
+                // TODO: Update member status
+                return;
+            }
+
+            return await knex.insert({ memberId, appointmentId }).into("appointment_members");
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+
+    async createNewAppointmentInfo(appointment) {
+        try {
+            const {
+                eventName,
+                meetingDate,
+                hostId,
+                stores,
+                members
+            } = appointment;
+            const knex = this.client.connection;
+
+            const appointmentId = await this.createAppointment({
+                eventName,
+                meetingDate,
+                hostId
+            });
+            Promise.all([
+                this.createAppointmentStores(stores, appointmentId),
+                ...members.map(async (memberId) => {
+                    await this.upsertAppointmentMembers(memberId, appointmentId);
+                })
+            ]);
+            return appointmentId;
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+
+    // async getAppointmentList(appointment) {
+    //     try {
+    //         await knex
+    //         .raw("select ")
+    //     } catch (error) {
+            
+    //     }
+    // }
 }
