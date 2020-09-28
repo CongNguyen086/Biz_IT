c3e342a76a673fc88de9bf93b43b6f3e67565542
Cong Nguyen <congnt@dgroup.co>
2020-09-28 19:03:52 +0700

    Config host app on expo

--- React Native/constants/config.js
+++ React Native/constants/config.js
@@ -1,6 +1,6 @@
 export default {
-	ROOT : 'http://localhost:3000',
+	ROOT : 'http://ec2-13-229-235-35.ap-southeast-1.compute.amazonaws.com',
 	API_ID : '795764707522976',
 	API_KEY : 'AIzaSyC6xFmp7UzfruFlCKb8QWyDBvA3lxNcSlQ',
-	SERVER_SOCKET_URL: 'http://localhost:3000'
+	SERVER_SOCKET_URL: 'http://ec2-13-229-235-35.ap-southeast-1.compute.amazonaws.com'
 }
